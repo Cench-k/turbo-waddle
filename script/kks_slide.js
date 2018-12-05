805b4517d423357e3f859a2c6551c3954e048abf
Cench-k <45585245+Cench-k@users.noreply.github.com>
2018-12-05 11:37:51 +0900

    text

---
+++ script/kks_slide.js
@@ -0,0 +1,15 @@
+$(window).onload(function(){
+
+});
+
+function kks_slide(){
+	var pofile_no = 0;
+	var rolling_p = null;
+
+	pofile_next = function(){
+		if (pofile_no < $(""))
+		{
+		}
+	}
+	
+};
