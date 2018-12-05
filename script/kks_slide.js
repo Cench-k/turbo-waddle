f3dfcc3d2573757f7019f78f46fbe9683d70fa14
Cench-k <45585245+Cench-k@users.noreply.github.com>
2018-12-05 15:45:45 +0900

    ..

--- script/kks_slide.js
+++ script/kks_slide.js
@@ -12,17 +12,17 @@ $(document).ready(function(){
 		}
 		$(".slide_box .port").fadeOut(500);
 		$(".slide_box .port:eq(" + pofile_no + ")").fadeIn(500);
-		//rolling_psetTimeout("pofile_next()",50000);
+		rolling_p = setTimeout("pofile_next()",50000);
 
 	};
 
 	$(".slide_box .next").click(function(){
-		//clearTimeout(rolling_p);
+		clearTimeout(rolling_p);
 		pofile_next();
 	});
 	
 	$(".slide_box .prev").click(function(){
-		//clearTimeout(rolling_p);
+		clearTimeout(rolling_p);
 		if (pofile_no > 0){
 			pofile_no--
 		}else{
@@ -30,7 +30,7 @@ $(document).ready(function(){
 		}
 		$(".slide_box .port").fadeOut(500);
 		$(".slide_box .port:eq(" + pofile_no + ")").fadeIn(500);
-		//rolling_psetTimeout("pofile_next()",50000);
+		rolling_p = setTimeout("pofile_next()",50000);
 	});
 
 });
