c140b91a1863618fbf90460c6192fef5e17a23f0
Cench-k <45585245+Cench-k@users.noreply.github.com>
2018-12-05 14:46:55 +0900

    slide

--- script/kks_slide.js
+++ script/kks_slide.js
@@ -1,5 +1,5 @@
 $(window).onload(function(){
-
+	kks_slide();
 });
 
 function kks_slide(){
@@ -7,9 +7,14 @@ function kks_slide(){
 	var rolling_p = null;
 
 	pofile_next = function(){
-		if (pofile_no < $(""))
-		{
+		if (pofile_no < $(".slide_box .port").length-1){
+			pofile_no++
+		} else {
+			pofile_no = 0
 		}
+		$(".slide_box .port").fadeOut(500);
+		$(".slide_box .port:eq(" + pofile_no + ")").fadeIn(500);
+
 	}
 	
 };
