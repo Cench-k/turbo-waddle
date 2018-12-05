3b82351cef2b76feaa9319fe082c8a7ca37c9126
Cench-k <45585245+Cench-k@users.noreply.github.com>
2018-12-05 15:27:49 +0900

    버튼업데이트

--- script/kks_slide.js
+++ script/kks_slide.js
@@ -18,6 +18,18 @@ $(window).onload(function(){
 	$(".slide_box .next").click(function(){
 		clearTimeout(rolling_p);
 		pofile_next();
-	})
+	});
+	
+	$(".slide_box .prev").click(function(){
+		clearTimeout(rolling_p);
+		if (pofile_no > 0){
+			pofile_no--
+		}else{
+			pofile_no = $(".slide_box .port").length-1)	
+		}
+		$(".slide_box .port").fadeOut(500);
+		$(".slide_box .port:eq(" + pofile_no + ")").fadeIn(500);
+		rolling_psetTimeout("pofile_next()",50000);
+	});
 
 });	
