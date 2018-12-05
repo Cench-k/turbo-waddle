1ef2f5e71834c715cbe9ceb160ac71b27044cc2d
Cench-k <45585245+Cench-k@users.noreply.github.com>
2018-12-05 15:22:02 +0900

    넥스트버튼

--- script/kks_slide.js
+++ script/kks_slide.js
@@ -11,7 +11,13 @@ $(window).onload(function(){
 		}
 		$(".slide_box .port").fadeOut(500);
 		$(".slide_box .port:eq(" + pofile_no + ")").fadeIn(500);
+		rolling_psetTimeout("pofile_next()",50000);
 
 	};
+
+	$(".slide_box .next").click(function(){
+		clearTimeout(rolling_p);
+		pofile_next();
+	})
 
 });	
