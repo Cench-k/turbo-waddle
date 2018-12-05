530081eb75d2763306ec338e3bd77b14d1214137
Cench-k <45585245+Cench-k@users.noreply.github.com>
2018-12-05 15:30:49 +0900

    ..

--- script/kks_slide.js
+++ script/kks_slide.js
@@ -25,7 +25,7 @@ $(window).onload(function(){
 		if (pofile_no > 0){
 			pofile_no--
 		}else{
-			pofile_no = $(".slide_box .port").length-1)	
+			pofile_no = $(".slide_box .port").length-1
 		}
 		$(".slide_box .port").fadeOut(500);
 		$(".slide_box .port:eq(" + pofile_no + ")").fadeIn(500);
