ab2241eeacbcadf0a443161dceecd7fbc5c5b758
Cench-k <45585245+Cench-k@users.noreply.github.com>
2018-12-10 11:17:24 +0900

    ..

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="ko">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="Generator" content="EditPlus®">
+		<meta name="Author" content="">
+		<meta name="Keywords" content="">
+		<meta name="Description" content="">
+		<title>Kim's PortFolio</title>
+		
+		<link rel="stylesheet" href="css/content.css" type="text/css">
+		<link rel="stylesheet" href="css/jquery-ui.css" type="text/css">
+		<script type="text/javascript" src="script/jquery-1.6.2.min.js"></script>
+		<script type="text/javascript" src="script/jquery-ui.js"></script>
+		<script type="text/javascript" src="script/html5.js"></script>	
+		<script type="text/javascript" src="script/kks_slide.js"></script>
+	</head>
+	<body>
+		<div class="wrap">
+			<div class="slide_box">
+				<div class="box">
+					<div class="port">
+						<h1 class="title">ProFile</h1>
+					</div>
+					<div class="port view"></div>
+					<div class="port view"></div>
+					<div class="port view">
+						<div class="list_area">
+							<div class="list_div" >
+								<div class="test">
+									<img src="img/2pg/1.jpg" />
+								</div>
+								<div class="test">
+									<img src="img/2pg/2.jpg" />
+								</div>
+								<div class="test">
+									<img src="img/2pg/3.jpg" />
+								</div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+								<div class="test"></div>
+							</div>
+						</div>
+					</div>
+					<div class="port view"></div>
+				</div>
+				<div class="prev"></div>
+				<div class="next"></div>
+				<div class="thum_box">
+					<ul class="bottom_list">
+						<li class="on">Profile</li>
+						<li>B</li>
+						<li>C</li>
+						<li>D</li>
+						<li>E</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<script type="text/javascript">
+			$(document).ready(function(){
+				resize_wrap();
+				$(window).resize(function(){
+					resize_wrap();
+				});
+			});
+
+			function resize_wrap(){
+				var wrap_height = $(window).height();
+				$(".wrap").height(wrap_height);
+				$(".slide_box").height(wrap_height);
+				$(".box").height(wrap_height);
+			}
+
+			
+			
+		</script>
+
+
+	</body>
+</html>
