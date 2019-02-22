a6fd267b5797a3f3066f89f89547babfb1a0b318
Cench-k <45585245+Cench-k@users.noreply.github.com>
2019-02-22 17:55:49 +0900

    tt

--- script/link.js
+++ script/link.js
@@ -2,8 +2,81 @@ function Gosite(link_go) {
 	switch ( link_go ) {
 
 		//
-		case 'site_1' : //
+		case 'site_1' : // 비긴어게인
 		window.open('http://www.beginagain.co.kr'); break;
+		case 'site_2' : // 별담펜션
+		window.open('http://www.xn--jk1b29p.com'); break;
+		case 'site_3' : // 천마에코피아
+		window.open('http://www.cheonmaecopia.com'); break;
+		case 'site_4' : // 나사로간호학원
+		window.open('http://www.jejunasaro.co.kr'); break;
+		case 'site_5' : // 재주수눌음지역자활센터
+		window.open('http://www.jejuhope.org'); break;
+		case 'site_6' : // 트리플에스
+		window.open('http://www.triples-s.com'); break;
+		case 'site_7' : // 늘하우스
+		window.open('http://www.alwayshouse.com'); break;
+		case 'site_8' : // 제주편한대형택시
+		window.open('http://www.xn--vk1bu41af2dvzag20ajje9odlwb.com'); break;
+		case 'site_9' : // 에즈빌리지
+		window.open('http://www.ezevillage.co.kr'); break;
+		case 'site_10' : // 외양간
+		window.open('http://www.xn--s39an89b1ta.com'); break;
+		case 'site_11' : // 빌클린
+		window.open('http://www.billclean.co.kr'); break;
+		case 'site_12' : // 재주특별자치도철인3종협회
+		window.open('http://www.jejutriathlon.com'); break;
+		case 'site_13' : // 별표코엑스
+		window.open('http://www.starcoex.co.kr'); break;
+		case 'site_14' : // 오현개발
+		window.open('http://www.o-hyeon.com'); break;
+		case 'site_15' : // 제주타라하우스
+		window.open('http://www.jejuthara.com'); break;
+		case 'site_16' : // 연동유치원
+		window.open('http://www.e-ini.co.kr'); break;
+		case 'site_17' : // MJ리조트
+		window.open('http://www.mjresort.co.kr'); break;
+		case 'site_18' : // 협재해변펜션
+		window.open('http://www.xn--6l3b27f2sgkynnhc40a.com'); break;
+		case 'site_19' : // 여여민박
+		window.open('http://www.cozytown.co.kr'); break;
+		case 'site_20' : // 김기수안과
+		window.open('http://www.jejulasik.com'); break;
+		case 'site_21' : // 코지펜션
+		window.open('http://www.jejucapepension.com'); break;
+		case 'site_22' : // 쉼펜션
+		window.open('http://www.jejushim.com'); break;
+		case 'site_23' : // 블루마운틴커피랜드앤리조트
+		window.open('http://www.resortbluemountain.com'); break;
+		case 'site_24' : // 헬스케어타운
+		window.open('http://www.jejuhctr.com'); break;
+		case 'site_25' : // 서귀포홀로사는 노인지원센터
+		window.open('http://www.sgpalone.co.kr'); break;
+		case 'site_26' : // 그린피플
+		window.open('http://www.jejugreenpeople.com'); break;
+		case 'site_27' : // 메이더카라반
+		window.open('http://www.meithecaravan.com'); break;
+		case 'site_28' : // 제이뷰호텔
+		window.open('http://www.jviewhotel.com'); break;
+		case 'site_29' : // 아라개발
+		window.open('http://www.chejuara.co.kr'); break;
+		case 'site_30' : // 유니텍코리아
+		window.open('http://www.untkorea.com '); break;
+		case 'site_31' : // 다인리조트
+		window.open('http://www.dyneresort.co.kr'); break;
+		case 'site_32' : // 제이디홈플랜
+		window.open('http://www.jdhomeplan.com'); break;
+		case 'site_33' : // 제주해양수산정책포럼
+		window.open('http://www.jofpf.kr'); break;
+		case 'site_34' : // 2대약초건강원
+		window.open('http://www.llooll.co.kr'); break;
+		case 'site_35' : // ING네일
+		window.open('http://www.ingnail.com'); break;
+		case 'site_36' : // 놀이마당얼쑤사회적협동조합
+		window.open('http://www.noricoop.com'); break;
+		case 'site_37' : // 재일영농조합법인
+		window.open('http://www.xn--439as8al6ewv5a7fa85zw1orrb.com'); break;
+
 
 
 
